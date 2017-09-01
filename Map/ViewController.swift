8d5913c13296ca9cd1b3b2776dfc4e8b34ddffe4
Roberto Carlos <roberto.oliveira@dinda.com.br>
2017-09-01 17:05:20 -0300

    initial code

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    let geoCoder = CLGeocoder()
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBAction func toggleMaptype(sender: UISegmentedControl!) {
+        if sender.selectedSegmentIndex == 0 {
+            self.mapView.mapType = MKMapType.standard
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            self.mapView.mapType = MKMapType.satellite
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            self.mapView.mapType = MKMapType.hybrid
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let address = "Avenida Rebouças, 3506 - São Paulo-SP"
+        let title   = "Origem"
+        
+        searchAddress(address: address, title: title, zoomDegre: 0.005)
+    }
+    
+    func searchAddress(address: String, title: String, zoomDegre: Double) {
+        geoCoder.geocodeAddressString(address) { (placemarks, error) in
+            guard let placemarks = placemarks else {return}
+            
+            if let location = placemarks.last?.location! {
+                self.printLocation(location: location, title: title, zoomDegre: zoomDegre)
+            }
+        }
+    }
+    
+    func printLocation(location: CLLocation, title: String, zoomDegre: Double) {
+        let coordinate = location.coordinate
+        NSLog("Location found from Map: %f %f", coordinate.latitude, coordinate.longitude)
+        
+        let span = MKCoordinateSpanMake(zoomDegre, zoomDegre)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        self.mapView.setRegion(region, animated: true)
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = title
+        self.mapView.addAnnotation(annotation)
+        
+        let camera = MKMapCamera(lookingAtCenter: coordinate, fromDistance: 1, pitch: 1, heading: 1)
+        self.mapView.setCamera(camera, animated: true)
+        self.mapView.showsUserLocation = true;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchAddress(address: searchBar.text!, title: "Destino", zoomDegre: 0.005)
+    }
+}
